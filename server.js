11c1522d76954418aa411f0bac2e86a9be0571e4
Farzad <farzadrahman59@gmail.com>
2018-06-21 01:41:45 +0600

    Farzad

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
 
-app.listen(8080);
+app.listen(3000);
+
 
 function handler (req, res) {
     fs.readFile(__dirname + '/index.html',
@@ -33,8 +34,8 @@ io.on('connection', function(socket){
 io.on('connect', (socket) => {
     numUsers++;
     console.log("Client Connected");
-    console.log(socket.id);
-    console.log("Total Users: "+numUsers);
+    // console.log(socket.id);
+    // console.log("Total Users: "+numUsers);
 
 });
 io.on('disconnect', (socket) => {
